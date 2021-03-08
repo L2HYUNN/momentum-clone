04f2e635c4e5e30c8e0aea16c7a3348d39aaf47e
L2HYUNN <dhl9810@naver.com>
2021-03-08 17:59:35 +0900

    #3.3 Saving the User Name part Two

--- gretting.js
+++ gretting.js
@@ -5,6 +5,27 @@ greetings = document.querySelector(".js-greetings");
 const USER_LS = "currentUser",
     SHOWING_CN = "showing"
 
+function saveName(text){
+    localStorage.setItem(USER_LS, text);    
+}
+
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    // localStorage.setItem("currentUser", currentValue);
+    // form.classList.remove(SHOWING_CN);
+    // loadName();
+    paintGreeting(currentValue);
+    saveName(currentValue);
+
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit",handleSubmit);
+}
+
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greetings.classList.add(SHOWING_CN);
@@ -14,6 +35,8 @@ function paintGreeting(text){
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
+        askForName();
+        
 
     } else {
         paintGreeting(currentUser);
