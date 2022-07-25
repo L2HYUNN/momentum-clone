830840f8cb022ca4386581731e66b228bdd0c7d8
L2HYUNN <dhl9810@naver.com>
2022-07-25 23:06:14 +0900

    add new weather

--- weather.js
+++ weather.js
@@ -1,39 +1,36 @@
 const API_KEY = "f7f676014d402a425cf681bdbc4b77de";
-const COORDS = 'coords';
+const COORDS = "coords";
 
-function saveCoords(coordsObj){
-    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
-function handleGeoSucces(position){
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    const coordsObj = {
-        latitude,
-        longitude
-    };
-    saveCoords(coordsObj);
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
 }
 
-function handleGeoError(){
+function handleGeoError() {}
 
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+function loadCoords() {
+  const loadedCords = localStorage.getItem(COORDS);
+  if (loadedCords === null) {
+    askForCoords();
+  } else {
+    // getWeather
+  }
 }
 
-function askForCoords(){
-    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
-}
-function loadCoords(){
-    const loadedCords = localStorage.getItem(COORDS);
-    if(loadedCords === null){
-        askForCoords();
-    } else{
-        // getWeather
-    }
-}
-
-function init(){
-    loadCoords();
-
+function init() {
+  loadCoords();
 }
 
 init();
